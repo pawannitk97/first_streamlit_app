f8ca6448847299fc5b7ec312f9eaff932606cae1
Pawan Kumar Kumawat <44918373+pawannitk97@users.noreply.github.com>
2023-07-29 19:56:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
-streamlit.title('Sparsh Daily Healthy Dinner')
-streamlit.header('Today Menu')
-streamlit.text('Omlette paratha')
-streamlit.text('Daal rice')
-streamlit.text('sabji chapatti')
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
